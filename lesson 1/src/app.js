feed25dfbadcb7c5ed723e529b3bb156aa0fdfe4
PoulaHelmy <43091492+PoulaHelmy@users.noreply.github.com>
2022-03-18 12:20:49 +0200

    finish lesson 1

--- lesson 1/src/app.js
+++ lesson 1/src/app.js
@@ -1,9 +1,22 @@
-
+const Pet = (props) =>{
+  return (
+      React.createElement('div',{},[
+          React.createElement('h1',{},props.name),
+          React.createElement('h2',{},props.animal),
+          React.createElement('h3',{},props.breed),
+      ])
+  )
+}
 const App = () => {
   return React.createElement(
     'div',
       {},
-    React.createElement('h1', {}, 'Adopt Me!')
+      [
+        React.createElement('h1', {id:"brand"}, 'Adopt Me!'),
+        React.createElement(Pet,{name:'Mittens', animal:'cat', breed:'tabby'}),
+        React.createElement(Pet,{name:'Molly', animal:'dog', breed:'lab'}),
+        React.createElement(Pet,{name:'Milo', animal:'dog', breed:'beagle'}),
+      ]
   );
 };
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
